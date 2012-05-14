6bc0dc976b72a23cff271d3b77693c050569b228
Alexandre Bique <bique.alexandre@gmail.com>
2012-05-14 19:46:30 +0200

    Add the main interface of TorrentDb.

--- hefur/torrent-db.hh
+++ hefur/torrent-db.hh
@@ -5,6 +5,12 @@ namespace hefur
 {
   class TorrentDb
   {
+  public:
+    /**
+     * Execute the request and store the reponse in the return value.
+     * @warning You are responsible to delete the return value.
+     */
+    AnnounceResponse* annouce(const AnnounceRequest & request);
   };
 }
 
