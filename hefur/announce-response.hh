82de0bd248f69bb6f3f55fb1edf86d5790dbef92
Alexandre Bique <bique.alexandre@gmail.com>
2012-05-14 19:37:08 +0200

    Add internal AnnounceResponse representation.

--- hefur/announce-response.hh
+++ hefur/announce-response.hh
@@ -9,6 +9,14 @@ namespace hefur
    */
   struct AnnounceResponse
   {
+    bool                 error_;
+    std::string          error_msg_;
+
+    uint32_t             nleechers_;
+    uint32_t             nseeders_;
+    uint32_t             ncompleted_;
+    uint32_t             interval_;
+    std::vector<Address> addrs_;
   };
 }
 
