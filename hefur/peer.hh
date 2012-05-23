f6af349651358a97b98bd819587107b03763bcc8
Alexandre Bique <bique.alexandre@gmail.com>
2012-05-23 19:11:52 +0200

    Torrent: use the peerid as a key for the trie.

--- hefur/peer.hh
+++ hefur/peer.hh
@@ -13,17 +13,20 @@ namespace hefur
   class Peer
   {
   public:
-    Peer();
-
     Address       addr_;
     uint64_t      left_;
     uint64_t      downloaded_;
     uint64_t      uploaded_;
     uint32_t      timeout_ts_; // monotonic coarse clock
+    uint8_t       peerid_[20];
 
     mimosa::IntrusiveDListHook<Peer*> timeout_hook_;
 
-    static mimosa::StringRef key(Peer * peer) { return peer->addr_.key(); }
+    static inline mimosa::StringRef key(Peer * peer) {
+      // don't use the 8 first bytes as they're not really relevant:
+      // they represent the peer's client and version.
+      return mimosa::StringRef((const char *)peer->peerid_ + 8, 12);
+    }
   };
 }
 
