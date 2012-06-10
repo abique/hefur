a66ceb8323940c45355442b55ccacae52e3e4601
Alexandre Bique <bique.alexandre@gmail.com>
2012-06-10 14:59:32 +0200

    Add a namespace alias header

---
+++ hefur/namespace-helper.hh
@@ -0,0 +1,26 @@
+#ifndef HEFUR_NAMESPACE_HELPER_HH
+# define HEFUR_NAMESPACE_HELPER_HH
+
+namespace mimosa
+{
+  namespace bencode {}
+  namespace format {}
+  namespace http {}
+  namespace log {}
+  namespace options {}
+  namespace stream {}
+  namespace tpl {}
+}
+
+namespace hefur
+{
+  namespace m  = mimosa;
+  namespace mb = mimosa::bencode;
+  namespace mf = mimosa::format;
+  namespace mh = mimosa::http;
+  namespace ml = mimosa::log;
+  namespace mo = mimosa::options;
+  namespace ms = mimosa::stream;
+}
+
+#endif /* !HEFUR_NAMESPACE_HELPER_HH */
