add11216a85ca16be950fcca9b412668026cef67
Alexandre Bique <bique.alexandre@gmail.com>
2012-07-05 06:57:41 +0200

    Fix table sorter

--- www/js/table-sorter.js
+++ www/js/table-sorter.js
@@ -12,19 +12,48 @@ function parseByteSize(str) {
         .replace("MiB", "*1024*1024")
         .replace("KiB", "*1024")
         .replace("B", "");
-    return eval(str);
+    return parseInt(eval(str));
 }
 
-function sortTbody(tbody, col, lt) {
+function sortTbody2(tbody, col, lt) {
     rows = []
     for (i = 0; i < tbody.rows.length; ++i)
-        rows.push(tbody.rows[i]);
+        rows.push(tbody.rows[i])
     rows = rows.sort(function (a, b) {
         return lt(a.cells[col].textContent, b.cells[col].textContent);
-        });
+    });
     while (tbody.rows.length > 0)
         tbody.deleteRow(0);
     for (i in rows)
         tbody.appendChild(rows[i]);
     return rows;
+}
+
+function sortTbody(tbody_id, lt) {
+    tbody = document.getElementById(tbody_id);
+    col = this.event.srcElement.cellIndex;
+    return sortTbody2(tbody, col, lt);
+}
+
+function sortStr(a, b) {
+    return a > b;
+}
+
+function sortInt(a, b) {
+    return parseInt(a) < parseInt(b);
+}
+
+function sortByte(a, b) {
+    return parseByteSize(a) < parseByteSize(b);
+}
+
+function getSortMethod(elt) {
+    for (i = 0; i < elt.classList.length; ++i)
+        if (elt.classList.item(i))
+            ;
+}
+
+function tutu() {
+    msg = "col: " + this.event.srcElement.cellIndex;
+    alert(msg);
 }
