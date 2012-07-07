f7794e5f9b3308fc6793ab6da22bc3a6c01db915
Alexandre Bique <bique.alexandre@gmail.com>
2012-07-07 05:59:10 +0200

    Update mimosa

--- hefur/udp-server.hh
+++ hefur/udp-server.hh
@@ -143,7 +143,7 @@ namespace hefur
 
     struct SendToBuffer
     {
-      m::IntrusiveSlistHook<SendToBuffer *> lh_; // list hook
+      m::IntrusiveSlistHook<SendToBuffer> lh_; // list hook
 
       union                     // sendto addr
       {
@@ -183,7 +183,7 @@ namespace hefur
 
     static hefur::AnnounceRequest::Event convert(Event event);
 
-    typedef m::IntrusiveSlist<SendToBuffer, SendToBuffer *, &SendToBuffer::lh_> sbufs_type;
+    typedef m::IntrusiveSlist<SendToBuffer, &SendToBuffer::lh_> sbufs_type;
 
     bool       stop_;
     m::Thread  thread_;
